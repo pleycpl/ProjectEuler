3efb7598b47cc591ec22b144bf5698b36de636a5
pleycpl <h4rvey@protonmail.com>
2016-11-19 23:32:23 +0200

    Update 002-EvenFibonacciNumbers.js

--- src/002-EvenFibonacciNumbers.js
+++ src/002-EvenFibonacciNumbers.js
@@ -2,11 +2,10 @@ var i = 1;
 var j = 2;
 var array = [];
 while(j < 10) {
-    if (j % 2 === 0) {
-	array.push(j);
-    }
-    i = j-i+(j=i);
-    j += i;
+  if (j % 2 === 0) {
+    array.push(j);
+  }
+  i = j-i+(j=i); // swapping i and j
+  j += i;
 }
-
 console.log(array);
